52a5866ce8bb567b3997d5f62cf6e702b59f04f5
edouardewans <edouardewans@80dd49f9-2940-0410-a5c2-6350f62ab172>
2007-12-03 14:47:21 +0000

    supression de Curves

--- src/function/FunVariations.java
+++ src/function/FunVariations.java
@@ -3,6 +3,10 @@ package function;
 import curves.Variations;
 
 public class FunVariations extends Variations {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 814792668273453011L;
 	private Function f;
 
 	public FunVariations(Function f, double xmin, double xmax) {
